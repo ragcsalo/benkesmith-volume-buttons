552e68c98528b1841c27502ec19c7cdc2c0675c3
Bence Kovacs <kov.benc@gmail.com>
2025-05-03 17:41:41 +0200

    Update volume-buttons.js

--- www/volume-buttons.js
+++ www/volume-buttons.js
@@ -1,9 +1,13 @@
 var exec = require('cordova/exec');
 
 var VolumeButtons = {
-    onVolumeButtonPressed: function(callback, errorCallback) {
-        exec(callback, errorCallback, 'VolumeButtons', 'onVolumeButtonPressed', []);
-    }
+  onVolumeButtonPressed: function(success, failure) {
+    exec(success, failure, 'VolumeButtons', 'onVolumeButtonPressed', []);
+  },
+  setMonitoringMode: function(mode, success, failure) {
+    // mode: "aggressive" | "silent" | "none"
+    exec(success, failure, 'VolumeButtons', 'setMonitoringMode', [mode]);
+  }
 };
 
 module.exports = VolumeButtons;
