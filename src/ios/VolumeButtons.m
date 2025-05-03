0ca7928df60b9fd89f30013f87527e067f7124f2
Bence Kovacs <kov.benc@gmail.com>
2025-05-03 13:01:13 +0200

    Update VolumeButtons.m

--- src/ios/VolumeButtons.m
+++ src/ios/VolumeButtons.m
@@ -1,5 +1,6 @@
 #import "VolumeButtons.h"
 #import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation VolumeButtons {
     MPVolumeView *volumeView;
@@ -10,20 +11,48 @@
 - (void)pluginInitialize {
     [super pluginInitialize];
     
+    NSLog(@"VolumeButtons: Initializing plugin");
+
     volumeView = [[MPVolumeView alloc] initWithFrame:CGRectZero];
-    [[UIApplication sharedApplication].keyWindow addSubview:volumeView];
+    UIWindow *mainWindow = [self getMainWindow];
+    [mainWindow addSubview:volumeView];
     volumeView.hidden = YES;
 
     AVAudioSession *audioSession = [AVAudioSession sharedInstance];
     [audioSession setActive:YES error:nil];
     initialVolume = audioSession.outputVolume;
-    
+
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(volumeChanged:)
                                                  name:@"AVSystemController_SystemVolumeDidChangeNotification"
                                                object:nil];
 
     [self createInvisibleAudioPlayer];
+}
+
+- (UIWindow *)getMainWindow {
+    UIWindow *mainWindow = nil;
+    if (@available(iOS 13.0, *)) {
+        for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive) {
+                mainWindow = scene.windows.firstObject;
+                break;
+            }
+        }
+    }
+    if (!mainWindow) {
+        if (@available(iOS 15.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    mainWindow = [scene.windows firstObject];
+                    break;
+                }
+            }
+        } else {
+            mainWindow = [UIApplication sharedApplication].windows.firstObject;
+        }
+    }
+    return mainWindow;
 }
 
 - (void)createInvisibleAudioPlayer {
@@ -36,6 +65,9 @@
 }
 
 - (void)volumeChanged:(NSNotification *)notification {
+    NSLog(@"VolumeButtons: Volume change detected");
+    NSDictionary *userInfo = [notification userInfo];
+    NSLog(@"VolumeButtons: UserInfo %@", userInfo);
     if (!volumeButtonPressed) {
         volumeButtonPressed = YES;
         [self resetVolume];
@@ -45,15 +77,33 @@
 }
 
 - (void)resetVolume {
-    MPMusicPlayerController *mpc = [MPMusicPlayerController applicationMusicPlayer];
-    [mpc setVolume:initialVolume];
+    NSLog(@"VolumeButtons: Resetting volume");
+    UISlider *volumeSlider = nil;
+    for (UIView *view in volumeView.subviews) {
+        if ([view isKindOfClass:[UISlider class]]) {
+            volumeSlider = (UISlider *)view;
+            break;
+        }
+    }
+    if (volumeSlider != nil) {
+        [volumeSlider setValue:initialVolume animated:NO];
+    } else {
+        NSLog(@"VolumeButtons: Volume slider not found");
+    }
 }
 
 - (void)sendVolumeButtonPressed {
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:self.callbackId];
+    NSLog(@"VolumeButtons: Sending volume button press event to JavaScript");
+    if (self.callbackId != nil) {
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+    } else {
+        NSLog(@"VolumeButtons: Callback ID is nil");
+    }
 }
 
-- (void)onVolumeButtonPressed:(CDVInvokedUrlCommand*)command {
+- (void)onVolumeButtonPressed:(CDVInvokedUrlCommand *)command {
+    NSLog(@"VolumeButtons: JavaScript registered for volume button press event");
     self.callbackId = command.callbackId;
 }
 
